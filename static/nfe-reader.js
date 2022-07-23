79863914dd5fa0dbedad8fe9ab214b29f3e8f0a2
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2022-07-23 02:40:38 -0300

    simplify styling logic

--- static/nfe-reader.js
+++ static/nfe-reader.js
@@ -16,61 +16,11 @@ var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 
 html5QrcodeScanner.render(onScanSuccess, onScanError);
 
 
-function applyStyleToButtons() {    
-    for (let item of document.getElementsByTagName("button")) {
-        console.log("Applying style to ", item)
-        item.className="btn btn-outline-info";
-        item.type="button";
-    }
-  }
-
-  function applyStyleToRequiredButtons() {    
-    var requiredButtons = ["Start Scanning", "Stop Scanning"]
-
-    for (let item of document.getElementsByTagName("button")) {
-        console.log("Applying style to ", item)
-        item.className="btn btn-outline-info";
-        item.type="button";
-
-        if (requiredButtons.includes(item.textContent)){
-            requiredButtons.pop(requiredButtons.indexOf(item.textContent))
-        }
-    }
-
-    if (requiredButtons.length > 1) {
-        throw new Error("None of required buttons " + requiredButtons + " waren't available for style changes.")
-    }
-  }
-
-
-function fixReaderDiv() {
-    document.getElementById("reader").style=""
-    document.getElementById('reader__dashboard_section_csr').style=""
-}
-
-
-function fixScanButton() {
-    var scanButton = document.getElementById("reader__camera_permission_button")
-    if (scanButton) {
-        scanButton.textContent = "Scan"
-        scanButton.addEventListener("click", () => {
-            runWithRetries(()=>{
-                stopLoader()
-                applyStyleToRequiredButtons()
-            });
-        });
-    }
-    
-}
-
 
 function stopLoader(counter) {
     document.getElementById("wrapper").className = "wrapper"
     document.getElementById("loader").style = "display:none"
 }
-
-
-
 
 function startLoader(counter) {
     document.getElementById("wrapper").className = "blur wrapper"
@@ -97,8 +47,40 @@ function runWithRetries(func, numOfRetries = 20) {
 }
 
 
-runWithRetries(applyStyleToButtons)
-runWithRetries(fixScanButton)
-runWithRetries(fixReaderDiv)
-runWithRetries(stopLoader)
+stopLoader()
 
+
+function fixScanButton() {
+    var scanButton = document.getElementById("reader__camera_permission_button")
+    if (scanButton) {
+        scanButton.textContent = "Scan"
+    }
+    
+}
+
+function fixReaderDiv() {
+    document.getElementById("reader").style=""
+    document.getElementById('reader__dashboard_section_csr').style=""
+}
+
+function styleButtons() {
+    for (let item of document.getElementsByTagName("button")) {    
+        if (item.className != "btn btn-outline-info") {
+            item.className="btn btn-outline-info";
+            item.type="button";
+        }
+    }
+}
+
+function forEver(){
+    try {
+        styleButtons()
+        fixScanButton()
+    } catch(e){}
+
+    setTimeout(() => {
+        forEver()
+    }, 500);
+}
+
+forEver()
