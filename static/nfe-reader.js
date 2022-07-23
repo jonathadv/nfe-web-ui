f9fb797d0fee7f1ba8d51693ac16c4d6cd8df963
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2022-07-23 00:00:04 -0300

    improve style

--- static/nfe-reader.js
+++ static/nfe-reader.js
@@ -1,27 +1,7 @@
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
     console.log(`Scan result: ${decodedText}`, decodedResult);
-}
-
-var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
-
-html5QrcodeScanner.render(onScanSuccess);
-
-var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
-
-function onScanSuccess(decodedText, decodedResult) {
-    // Handle on success condition with the decoded text or result.
-    console.log(`Scan result: ${decodedText}`, decodedResult);
-    // ...
     html5QrcodeScanner.clear();
-    // ^ this will stop the scanner (video feed) and clear the scan area.
-}
-
-html5QrcodeScanner.render(onScanSuccess);
-
-function onScanSuccess(decodedText, decodedResult) {
-    // Handle on success condition with the decoded text or result.
-    console.log(`Scan result: ${decodedText}`, decodedResult);
 
     document.getElementById('nfe-url-input').value=decodedResult.decodedText
     document.getElementById('nfe-form-input').submit()
@@ -29,14 +9,77 @@ function onScanSuccess(decodedText, decodedResult) {
 
 function onScanError(errorMessage) {
     // handle on error condition, with error message
+    console.log("Error while reading: ", errorMessage);
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
 
 
+function applyStyle() {
+    console.log("###  applyStyle")
+    try {
+        for (let item of document.getElementsByTagName("button")) {
+            if (item.type != "button"){
+                item.className="btn btn-outline-info";
+                item.type="button";
+            }        
+        }
+    } catch(err){
+        alert(err)
+    }        
+}
+    
 
-document.getElementById("reader__camera_permission_button").className="btn btn-primary js-tooltip"
-document.getElementById("reader__dashboard_section_swaplink").className="btn btn-primary js-tooltip"
-document.getElementById("reader__dashboard_section_swaplink").style="margin: 10px"
+function fixReaderDiv() {
+    try {
+        document.getElementById("reader").style=""
+        document.getElementById('reader__dashboard_section_csr').style=""
+    } catch(err){
+        alert(err)
+    }        
+}
 
+
+function fixScanButton() {
+    try {
+        var scanButton = document.getElementById("reader__camera_permission_button")
+        scanButton.textContent = "Scan"
+        scanButton.addEventListener("click", () => {
+            startLoader()
+            setTimeout(() => {
+                stopLoader()
+                applyStyle()
+            }, 3000);
+        })
+    } catch(err){
+        alert(err)
+    }        
+}
+
+
+function stopLoader() {
+    try {
+        document.getElementById("wrapper").className = "wrapper"
+        document.getElementById("loader").style = "display:none"
+    }catch(err){
+        alert(err)
+    }
+    
+}
+
+function startLoader() {
+    try {
+        document.getElementById("wrapper").className = "blur wrapper"
+        document.getElementById("loader").style = "display:block"
+    }catch(err){
+        alert(err)
+    }        
+}
+
+
+setTimeout(() => {
+    applyStyle()
+    fixScanButton()
+    stopLoader() 
+}, 3000);
